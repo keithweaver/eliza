36886bfd3c903e829deb546d02fb66ad5d221f55
Keith Weaver <keithmweaver@gmail.com>
2017-01-30 19:42:19 -0500

    The current status. Tried doing more than keyword match

--- js/eliza.js
+++ js/eliza.js
@@ -4,18 +4,22 @@ var responses = {
 					"I see.",
 					"I'm not sure I understand you fully.",
 					"Can you elaborate?",
-					"That is quite interesting."
+					"That is quite interesting.",
+					"Please tell me more.",
+					"Let's change focus a bit... Tell me about your family.",
+					"Can you elaborate on that?",
+					"Why do you say that *?"
 				],
 	"sorry" : ["Please don't apologize."],
 	"always" : ["Can you think of a specific example?"],
 	"because" : ["Is that the real reason?"],
 	"maybe" : ["You  don't seem very certain."],
 	"i think" : ["Do you really think so?"],
-	"you" : ["We were discussing you, not me."],
+	"you" : ["We were discussing you, not me.","Why do you say that about me?","Why do you care whether I \"*\"?"],
 	"yes" : ["Why do you think so?","You seem quite positive."],
 	"no" : ["Why not?","Are you sure?"],
-	"i am" : ["I am sorry to hear you are *.","How long have you been *?","Do you believe it is normal to be *?","Do you enjoy being *?"],
-	"i'm" : ["I am sorry to hear you are *.","How long have you been *?","Do you believe it is normal to be *?","Do you enjoy being *?"],
+	"i am" : ["I am sorry to hear you are *.","How long have you been *?","Do you believe it is normal to be *?","Do you enjoy being *?","Did you come to me because you are *?"],
+	"i'm" : ["I am sorry to hear you are *.","How long have you been *?","Do you believe it is normal to be *?","Do you enjoy being *?","Did you come to me because you are *?"],
 	"i feel" : ["Tell me more about such feelings.","Do you often feel *?","Do you enjoy feeling *?","Why do you feel that way?"],
 	"family" : ["Tell me more about your family.","How do you get along with your family?","Is your family important to you?"],
 	"mother" : ["Tell me more about your family.","How do you get along with your family?","Is your family important to you?"],
@@ -25,18 +29,19 @@ var responses = {
 	"brother" : ["Tell me more about your family.","How do you get along with your family?","Is your family important to you?"],
 	"husband" : ["Tell me more about your family.","How do you get along with your family?","Is your family important to you?"],
 	"wife" : ["Tell me more about your family.","How do you get along with your family?","Is your family important to you?"],
+	"child" : ["Did you have close friends as a child?","What is your favorite childhood memory?","Do you remember any dreams or nightmares from childhood?","Did the other children sometimes tease you?","How do you think your childhood experiences relate to your feelings today?"],
 	"dream" : ["What does that dream suggest to you?","Do you dream often?","What persons appear in your dreams?","Are you disturbed by your dreams?"],
 	"nightmare" : ["What does that dream suggest to you?","Do you dream often?","What persons appear in your dreams?","Are you disturbed by your dreams?"],
 	"hello" : ["Hi again! How is going?","How are you today? Any problems?"],
 	"good afternoon" : ["Hi again! How is going?","How are you today? Any problems?"],
 	"good morning" : ["Hi again! How is going?","How are you today? Any problems?"],
-	"hi" : ["Hi again! How is going?","How are you today? Any problems?"]
-	
+	"hi" : ["Hi again! How is going?","How are you today? Any problems?"],
+	"goodbye" : ["Goodbye.  Thank you for talking to me."],
+	"i need" : ["Why do you need *?","Would it really help you to get *?","Are you sure you need *?"],
+	"why don\'t you" : ["Do you really think I don't *?","Perhaps eventually I will *.","Do you really want me to *?"],
+	"why can\'t i" : ["Do you think you should be able to *?","If you could *, what would you do?","I don't know -- why can't you *?","Have you really tried?"],
+	"i can\'t" : ["How do you know you can't \"*\"?","Perhaps you could * if you tried.","What would it take for you to *?"]
 };
-var respVal = ["I am sorry to hear you are *.","How long have you been *?","Do you believe it is normal to be *?","Do you enjoy being *?"];
-
-
-
 var endChatTerms = ["goodbye","I have to leave","I have to leave.","quit","bye","exit"];
 
 var keywords = [];
@@ -77,8 +82,7 @@ function sendElizaNewMessage(newMessage){
 	clearSendTextbox();
 
 	//Make it lower case
-	newMessage = newMessage.toLowerCase();
-
+	newMessage = processInput(newMessage);
 	var response = analyze(newMessage);
 
 	displayChat();
@@ -89,6 +93,7 @@ function sendElizaNewMessage(newMessage){
 	}, determineResponseTime());
 
 }
+
 function analyze(newMessage){
 	var found = false;
 	var response = '';
@@ -117,8 +122,65 @@ function analyze(newMessage){
 
 	return response;
 }
+
+function processInput(message){
+	message = message.toLowerCase();
+	message = removePunctuation(message);
+	return message;
+}
+function replaceWords(newMessage){
+	var wordsForReplacement = [];
+	wordsForReplacement["i"] = "you";
+	wordsForReplacement["you"] = "i";
+	wordsForReplacement["me"] = "you";
+	wordsForReplacement["my"] = "your";
+	wordsForReplacement["am"] = "are";
+	wordsForReplacement["are"] = "am";
+	wordsForReplacement["was"] = "were";
+	wordsForReplacement["i'd"] = "you would";
+	wordsForReplacement["i've"] = "you have";
+	wordsForReplacement["i'll"] = "you will";
+	wordsForReplacement["you've"] = "i have";
+	wordsForReplacement["you'll"] = "i will";
+	wordsForReplacement["your"] = "my";
+	wordsForReplacement["yours"] = "mine";
+	wordsForReplacement["me"] = "you";
+	
+	for(var word in wordsForReplacement){
+		if(newMessage.indexOf(word) != -1 && newMessage.length == word.length){
+			newMessage = newMessage.replace(word);
+		}else if(newMessage.indexOf(" " + word) != 1){
+			newMessage = newMessage.replace(" " + word);
+		}else if(newMessage.indexOf(word + " ") != -1){
+			newMessage = newMessage.replace(word + " ");
+		}
+	}
+
+	return newMessage;
+}
+function removePunctuation(message){
+	//',;.?!:'
+	message = message.replace(",","");
+	message = message.replace(";","");
+	message = message.replace(".","");
+	message = message.replace("?","");
+	message = message.replace("!","");
+	message = message.replace(":","");
+	
+	
+	return message;
+}
 function determineResponseTime(){
 	var m = Math.floor(Math.random() * 2000) + 500;
 	console.log(m);
 	return m;
 }
+function compareKeywords(a,b) {
+	if (parseInt(a.weight) < parseInt(b.weight))
+		return -1;
+	if (parseInt(a.weight) > parseInt(b.weight))
+		return 1;
+	return 0;
+}
+
+
